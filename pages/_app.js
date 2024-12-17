06229f2c1bc739f15edc1b046987ea5fd5198f4f
Aditya Patadia <aditya@gumlet.com>
2024-12-17 15:17:44 +0530

    added pictures and logos

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import '../styles.css'
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    )
 }
