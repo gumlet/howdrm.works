3d8bd649c468563a74f94fa1f997134d3e67ece7
Aditya Patadia <aditya@gumlet.com>
2024-12-17 15:54:55 +0530

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import '../styles.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from "next/font/google";
+import '../styles.css'
+
 const inter = Inter({ subsets: ["latin"] });
+
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <main className={inter.className}>
+            <GoogleAnalytics gaId="G-QLCKRKMERW" />
             <Component {...pageProps} />
         </main>
     )
